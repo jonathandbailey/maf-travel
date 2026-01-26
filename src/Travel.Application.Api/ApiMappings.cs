01a5098bffeb61d932ccc1354a06a75a326474f2
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-01-26 08:55:17 +0100

    Split Api Mappings

--- src/Travel.Application.Api/ApiMappings.cs
+++ src/Travel.Application.Api/ApiMappings.cs
@@ -11,59 +11,39 @@ namespace Travel.Application.Api;
 public static class ApiMappings
 {
     private const string ApiConversationsRoot = "api";
-    private const string CreateSessionPath = "travel/plans/session";
-    private const string GetSessionPath = "travel/sessions/{sessionId}";
-    
-    private const string GetTravelPlanPath = "travel/plans/{travelPlanId}";
-    private const string UpdateTravelPlanPath = "travel/plans/{threadId}";
-    
+ 
+    public static void MapApi(this WebApplication app)
+    {
+        var api = app.MapGroup(ApiConversationsRoot);
+
+        api.MapSessionApi();
+        api.MapTravelApi();
+
+        api.MapFlightApi();
+    }
+}
+
+public static class FlightsApiMappings
+{
     private const string TravelFlightsSearchPath = "travel/flights/search/{threadId}";
     private const string TravelPlanFlightsSearchPath = "travel/flights/search/{threadId}";
     private const string GetTravelFlightsSearchPath = "travel/flights/search/{id}";
 
-    public static WebApplication MapApi(this WebApplication app)
+    public static void MapFlightApi(this RouteGroupBuilder api)
     {
-        var api = app.MapGroup(ApiConversationsRoot);
+        api.MapPost(TravelFlightsSearchPath, SaveFlightSearch);
+        api.MapGet(GetTravelFlightsSearchPath, GetFlightSearch);
 
-        api.MapPost(CreateSessionPath, SessionsApiMappings.CreateSession);
-        api.MapGet(GetTravelPlanPath,TravelApiMappings.GetTravelPlan);
-        api.MapGet(GetSessionPath, SessionsApiMappings.GetSession);
-        api.MapPost(UpdateTravelPlanPath, TravelApiMappings.UpdateTravelPlan);
-        api.MapPost(TravelFlightsSearchPath, FlightsApiMappings.SaveFlightSearch);
-        api.MapGet(GetTravelFlightsSearchPath, FlightsApiMappings.GetFlightSearch);
-
-        api.MapPut(TravelPlanFlightsSearchPath, FlightsApiMappings.SaveFlightSearchToTravelPlan);
-
-        return app;
-    }
-}
-
-public static class SessionsApiMappings
-{
-    public static async Task<Ok<SessionDto>> GetSession(Guid sessionId, IMediator mediator, HttpContext context)
-    {
-        var sessionDto = await mediator.Send(new GetSessionQuery(context.User.Id(), sessionId));
-
-        return TypedResults.Ok(sessionDto);
+        api.MapPut(TravelPlanFlightsSearchPath, SaveFlightSearchToTravelPlan);
     }
 
-    public static async Task<Ok<SessionDto>> CreateSession(IMediator mediator, HttpContext context)
-    {
-        var session = await mediator.Send(new CreateSessionCommand(context.User.Id()));
-
-        return TypedResults.Ok(session);
-    }
-}   
-
-public static class FlightsApiMappings
-{
-    public static async Task<Ok<FlightSearchDto>> GetFlightSearch(Guid id, IMediator mediator, HttpContext context)
+    private static async Task<Ok<FlightSearchDto>> GetFlightSearch(Guid id, IMediator mediator, HttpContext context)
     {
         var result = await mediator.Send(new GetFlightSearchQuery(context.User.Id(), id));
         return TypedResults.Ok(result);
     }
 
-    public static async Task<Ok<Guid>> SaveFlightSearch(
+    private static async Task<Ok<Guid>> SaveFlightSearch(
         [FromBody] FlightSearchDto flightSearch,
         Guid threadId,
         IMediator mediator,
@@ -74,7 +54,7 @@ public static class FlightsApiMappings
         return TypedResults.Ok(id);
     }
 
-    public static async Task<Ok> SaveFlightSearchToTravelPlan(
+    private static async Task<Ok> SaveFlightSearchToTravelPlan(
         [FromBody] FlightSearchDto flightSearch,
         Guid threadId,
         IMediator mediator,
@@ -87,9 +67,48 @@ public static class FlightsApiMappings
     }
 }
 
+public static class SessionsApiMappings
+{
+    private const string CreateSessionPath = "travel/plans/session";
+    private const string GetSessionPath = "travel/sessions/{sessionId}";
+
+    public static void MapSessionApi(this RouteGroupBuilder api)
+    {
+        api.MapPost(CreateSessionPath, CreateSession);
+        api.MapGet(GetSessionPath, GetSession);
+    }
+
+    private static async Task<Ok<SessionDto>> GetSession(Guid sessionId, IMediator mediator, HttpContext context)
+    {
+        var sessionDto = await mediator.Send(new GetSessionQuery(context.User.Id(), sessionId));
+
+        return TypedResults.Ok(sessionDto);
+    }
+
+    private static async Task<Ok<SessionDto>> CreateSession(IMediator mediator, HttpContext context)
+    {
+        var session = await mediator.Send(new CreateSessionCommand(context.User.Id()));
+
+        return TypedResults.Ok(session);
+    }
+}   
+
+
+
 public static class TravelApiMappings
 {
-    public static async Task UpdateTravelPlan(
+    private const string GetTravelPlanPath = "travel/plans/{travelPlanId}";
+    private const string UpdateTravelPlanPath = "travel/plans/{threadId}";
+
+    public static void MapTravelApi(this RouteGroupBuilder api)
+    {
+        api.MapSessionApi();
+
+        api.MapGet(GetTravelPlanPath, GetTravelPlan);
+        api.MapPost(UpdateTravelPlanPath, UpdateTravelPlan);
+    }
+
+    private static async Task UpdateTravelPlan(
         [FromBody] TravelPlanUpdateDto travelPlanUpdateDto,
         Guid threadId,
         HttpContext context,
@@ -98,7 +117,7 @@ public static class TravelApiMappings
         await mediator.Send(new UpdateTravelPlanCommand(context.User.Id(), threadId, travelPlanUpdateDto));
     }
 
-    public static async Task<Ok<TravelPlanDto>> GetTravelPlan(HttpContext context, Guid travelPlanId, IMediator mediator)
+    private static async Task<Ok<TravelPlanDto>> GetTravelPlan(HttpContext context, Guid travelPlanId, IMediator mediator)
     {
         var travelPlanDto = await mediator.Send(new GetTravelPlanQuery(context.User.Id(), travelPlanId));
 
