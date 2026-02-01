78e8538f98ce905bc3d43cc4234c36dd337717fe
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-01 15:34:40 +0100

    Add dedicated flights A2A Project

---
+++ src/Travel.Agents.A2A.Flights/Dto/FlightAgentRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Travel.Agents.A2A.Dto;
+
+public class FlightAgentRequest(string Skill, string context, FlightSearchDto flightSearchDto);
---
+++ src/Travel.Agents.A2A.Flights/Dto/FlightSearchDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Travel.Agents.A2A.Dto;
+
+public record FlightSearchDto(string Origin, string Destination, DateTimeOffset DepartureDate, DateTimeOffset ReturnDate);
