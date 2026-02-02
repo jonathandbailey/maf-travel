2831da6791556b3048dc4098f7452b9721608fb9
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-02 07:14:41 +0100

    Add parsing of Travel Plan SnapShot

---
+++ src/Travel.Agents.A2A.Planning/Dto/TravelDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Travel.Agents.A2A.Planning.Dto;
+
+public record TravelPlanDto(string? Origin, string? Destination, DateTime? StartDate, DateTime? EndDate, int? NumberOfTravelers);
---
+++ src/Travel.Agents.A2A.Planning/Services/PlanningTaskManager.cs
@@ -0,0 +1,42 @@
+﻿using A2A;
+using Travel.Agents.A2A.Planning.Extensions;
+using Travel.Agents.A2A.Shared.Services;
+
+namespace Travel.Agents.A2A.Planning.Services;
+
+public class PlanningTaskManager : IPlanningTaskManager
+{
+    private readonly IA2ACardService _cardService;
+    public ITaskManager TaskManager { get; } = new TaskManager();
+
+    public PlanningTaskManager(IA2ACardService cardService)
+    {
+        _cardService = cardService;
+ 
+        TaskManager.OnTaskCreated += OnTaskCreated;
+        TaskManager.OnAgentCardQuery += OnAgentCardQuery;
+    }
+
+    private Task<AgentCard> OnAgentCardQuery(string url, CancellationToken cancellationToken)
+    {
+        return _cardService.GetAgentCard(url);
+    }
+
+    private async Task OnTaskCreated(AgentTask agentTask, CancellationToken cancellationToken)
+    {
+        var travelPlan = agentTask.ExtractTravelPlanSnapshot();
+
+        var message = new AgentMessage
+        {
+            Role = MessageRole.Agent,
+            ContextId = agentTask.ContextId,
+        };
+
+        await TaskManager.UpdateStatusAsync(agentTask.Id, TaskState.Completed, message, final: true, cancellationToken);
+    }
+}
+
+public interface IPlanningTaskManager
+{
+    ITaskManager TaskManager { get; }
+}
