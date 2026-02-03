e4d6598f04cbc2b02e267e0e209915660c003adf
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-03 11:14:38 +0100

    remove legacy projects

--- ui/src/features/chat/domain/Conversation.ts
+++
@@ -1,7 +0,0 @@
-import type { ConversationThread } from "./ConversationThread";
-
-export interface Conversation {
-    id: string;
-    title: string;
-    threads: ConversationThread[];
-}
--- ui/src/features/chat/domain/Exchange.ts
+++
@@ -1,9 +0,0 @@
-import type { Status } from "./Status";
-import type { Message } from "./Message";
-
-export interface Exchange {
-    id: string;
-    user: Message;
-    assistant: Message;
-    status: Status[]
-}
