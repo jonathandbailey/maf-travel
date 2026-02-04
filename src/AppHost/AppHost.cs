c1cb7a1308101cf081aae2c1d6e1d8c56fd9fa0f
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-04 13:49:27 +0100

    Remove legacy projects (#58)

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -6,13 +6,6 @@ var storage = builder.AddAzureStorageServices();
 
 var blobs = builder.AddAzureBlobsServices(storage);
 
- builder.AddProject<Projects.Travel_Gateway>("travel-gateway");
-
-
-builder.AddProject<Projects.Travel_Workflows_A2A>("travel-workflows-a2a")
-.WithReference(blobs)
-.WaitFor(blobs);
-
 builder.AddProject<Projects.Travel_Application_Api>("travel-application-api")
 .WithReference(blobs)
 .WaitFor(blobs);
