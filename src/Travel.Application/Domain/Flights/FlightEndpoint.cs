5f29c914faeab61039a0deb424b7e9e8847a101a
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-09 14:50:08 +0100

    Refactor/application project removal (#62)
    * remove application projects
    
    * Add Tests Application Folder
    
    * Remove Projects

--- src/Travel.Application/Domain/Flights/FlightEndpoint.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Travel.Application.Domain.Flights;
-
-public class FlightEndpoint
-{
-    public string Airport { get; set; }
-    public string AirportCode { get; set; }
-    public DateTime Datetime { get; set; }
-}
--- src/Travel.Application/Domain/Flights/FlightPrice.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Travel.Application.Domain.Flights;
-
-public class FlightPrice
-{
-    public decimal Amount { get; set; }
-    public string Currency { get; set; }
-}
