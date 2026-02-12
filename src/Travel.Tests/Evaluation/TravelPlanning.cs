869b7e2e3d13790b42a096ca3b718a295a515088
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-12 13:40:32 +0100

    Update Travel Planning Tests

--- src/Travel.Tests/Evaluation/TravelPlanning.cs
+++ src/Travel.Tests/Evaluation/TravelPlanning.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Agents.AI.Workflows;
+﻿using FluentAssertions;
+using Microsoft.Agents.AI.Workflows;
 using Microsoft.Extensions.AI;
 using Moq;
 using Travel.Agents.Services;
@@ -14,16 +15,22 @@ public class TravelPlanning
     private const string Origin = "Zurich";
     private const string Destination = "Paris";
     private const int NumberOfTravelers = 2;
-    private readonly DateTime _departureDate = new(2026, 5, 1);
+    private static readonly DateTime DepartureDate = new(2026, 5, 1);
+
+    private static readonly DateTime ReturnDate = new(2026, 6, 15);
+
+    private readonly string _firstMessage =
+        $"I want to plan a trip from {Origin} to {Destination} on the {DepartureDate:dd.MM.yyyy}, for {NumberOfTravelers} people.";
+
+    private readonly string _secondMessage = $"My return date is {ReturnDate:dd.MM.yyyy}";
 
     [Fact]
     public async Task Test()
     {
         var travelPlanService = new Mock<ITravelPlanService>();
+        
         travelPlanService.Setup(x => x.GetTravelPlanAsync()).ReturnsAsync(new TravelPlanDto());
-
-        var events = new List<WorkflowEvent>();
-
+     
         var agentTemplateRepository = AgentHelper.CreateAgentTemplateRepository();
 
         var agentFactory = AgentHelper.CreateAgentFactory();
@@ -32,24 +39,26 @@ public class TravelPlanning
 
         var workflowService = new TravelWorkflowService(travelPlanService.Object, agentProvider);
 
-        var request = new TravelWorkflowRequest(
-            new ChatMessage(ChatRole.User,
-            $"I want to plan a trip from {Origin} to {Destination} on the {_departureDate:dd.MM.yyyy}, for {NumberOfTravelers} people."));
+        var request = new TravelWorkflowRequest(new ChatMessage(ChatRole.User, _firstMessage));
 
-        await foreach (var evt in workflowService.WatchStreamAsync(request))
-        {
-            events.Add(evt);
+        var events = await workflowService.WatchStreamAsync(request).ToListAsync();
 
-            switch (evt)
-            {
-                case TravelPlanUpdateEvent travelPlanUpdateEvent:
-                    break;
-            }
-        }
+        events.Should().ShouldHaveType<TravelPlanUpdateEvent>();
 
-        travelPlanService.Verify(x => x.Update(It.IsAny<TravelPlanDto>()), Times.Once);
+        events.Should().ShouldHaveType<RequestInfoEvent>();
 
-        Assert.Contains(events, @event => @event is TravelPlanUpdateEvent);
-        Assert.Contains(events, @event => @event is RequestInfoEvent);
+        var checkPoint = events.GetCheckpointInfo();
+
+        checkPoint.Should().NotBeNull();
+
+        workflowService = new TravelWorkflowService(travelPlanService.Object, agentProvider);
+
+        request = new TravelWorkflowRequest(new ChatMessage(ChatRole.User, _secondMessage));
+
+        events = await workflowService.WatchStreamAsync(request).ToListAsync();
+
+        events.Should().ShouldHaveType<TravelPlanUpdateEvent>();
+      
+        events.Should().ShouldHaveType<TravelPlanningCompleteEvent>();
     }
 }
