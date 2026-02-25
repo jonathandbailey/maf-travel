c9612b7c534ad2a88ea84afcefcbead21d4ea27a
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-02-25 11:58:07 +0100

    Add Thread Id to Information Resposne Node

--- src/Travel.Workflows/Nodes/InformationReponseNode.cs
+++ src/Travel.Workflows/Nodes/InformationReponseNode.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Agents.AI.Workflows;
 using Travel.Workflows.Common;
 using Travel.Workflows.Dto;
+using Travel.Workflows.Extensions;
 using Travel.Workflows.Telemetry;
 
 namespace Travel.Workflows.Nodes;
@@ -11,7 +12,9 @@ public partial class InformationResponseNode() : Executor(NodeNames.InformationR
     private async ValueTask HandleAsync(InformationResponse informationResponse, IWorkflowContext context,
         CancellationToken cancellationToken = default)
     {
-        using var activity = TravelWorkflowTelemetry.InvokeNode(NodeNames.InformationResponseNode, Guid.NewGuid());
+        var threadId = await context.GetThreadId(cancellationToken);
+
+        using var activity = TravelWorkflowTelemetry.InvokeNode(NodeNames.InformationResponseNode, threadId);
 
         await context.SendMessageAsync(new TravelPlanExtractCommand(informationResponse.Message), cancellationToken);
     }
